1252d15eda67f02c4735b6f55d997b77e6bdce12
Peter M Adrianov <peter.adrianov@gmail.com>
2026-09-01 17:52:59 +0300

    Bump rrubocop formula to 0.7.3.

--- Formula/rrubocop.rb
+++ Formula/rrubocop.rb
@@ -1,8 +1,8 @@
 class Rrubocop < Formula
   desc "Fast RuboCop-compatible Ruby linter in Rust"
   homepage "https://github.com/adrianov/rrubocop"
-  url "https://github.com/adrianov/rrubocop/archive/refs/tags/v0.7.2.tar.gz"
-  sha256 "ae072b6cfa625bdd259793403f30bac268f1201708ed18ca13f0ed1a5d7d8dcd"
+  url "https://github.com/adrianov/rrubocop/archive/refs/tags/v0.7.3.tar.gz"
+  sha256 "03eb0c1196d7b0dfb57f7609c7f7efe022f51361027496ab34e7d31134824e00"
   license "GPL-3.0-or-later"
   head "https://github.com/adrianov/rrubocop.git", branch: "main"
 
